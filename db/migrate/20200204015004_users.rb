5f8a6d01314c74c1c493d37b03164266290c6365
Alex Martinez <alexandr3martin3z@gmail.com>
2020-02-04 01:54:51 +0000

    Automatically backed up by Learn

--- db/migrate/20200204015004_users.rb
+++ db/migrate/20200204015004_users.rb
@@ -1,7 +1,7 @@
 class Users < ActiveRecord::Migration[5.1]
   def change
     def change
-      create_table :user do |t|
+      create_table :users do |t|
         t.string :username
         t.string :password
         t.integer :balance
