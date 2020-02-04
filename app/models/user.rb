0e1b50dd695b3dd1d3d96b772e998cd634289ac9
Alex Martinez <alexandr3martin3z@gmail.com>
2020-02-04 03:10:22 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,4 @@
 class User < ActiveRecord::Base
 
-  # attr_reader :username, :password, :balance
-
-  # def create(username, password, balance)
-  #   @username = username
-  #   @password = password
-  #   @balance = balance
-  # end
-
 
 end
